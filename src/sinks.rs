27059d9836f2f292b017989154fe362538accd38
ali-sultani <sultani.alishoja@gmail.com>
2023-06-14 11:34:01 -0400

    changed the counter for each BP

--- src/sinks.rs
+++ src/sinks.rs
@@ -12,17 +12,10 @@ pub fn prom_out(
 ) -> Result<PrometheusOperations, Error> {
     
     let mut prom_ops = PrometheusOperations::default();
-
-    let timestamp = map_missing_block_count.clone().timestamp;
-    let hash = map_missing_block_count.clone().hash;
-    let current_producer = map_missing_block_count.clone().current_producer;
-    let active_schedule = map_missing_block_count.clone().producers;
-
     
-    let labels = HashMap::from([("producer".to_string(), current_producer.clone()),
-        ("timestamp".to_string(), timestamp.clone()),
-        ("hash".to_string(), hash.clone()),
-        ("active_shedule".to_string(), active_schedule.join(", "))]);
+    let current_producer = map_missing_block_count.clone().current_producer;
+
+    let labels = HashMap::from([("producer".to_string(), current_producer.clone())]);
     prom_ops.push(Counter::from("blocks_produced").with(labels.clone()).inc());
 
     Ok(prom_ops)
