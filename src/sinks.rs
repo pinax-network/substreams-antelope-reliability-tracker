430551e323836a2330a11cdf0600cdbdf05e8abc
ali-sultani <sultani.alishoja@gmail.com>
2023-06-15 14:47:18 -0400

    modified the sink in order to isolate each data to facilitate grafana chart creation

--- src/sinks.rs
+++ src/sinks.rs
@@ -9,21 +9,15 @@ pub fn prom_out(block: Block) -> Result<PrometheusOperations, Error> {
     
     let mut prom_ops = PrometheusOperations::default();
 
-    let timestamp = block.clone().header.unwrap().timestamp.unwrap().to_string();
-    let hash = block.clone().id;
-
-    let mut producers_list: Vec<String> = Vec::new();
-    for producer in block.clone().active_schedule_v2.unwrap().producers {
-        producers_list.push(producer.account_name.clone());
-    }
-
-    let curr_producer = block.clone().header.unwrap().producer;
-
-    let labels = HashMap::from([("producer".to_string(), curr_producer.clone()), 
-                                ("timestamp".to_string(), timestamp.clone()), 
-                                ("hash".to_string(), hash.clone()),
-                                ("active_schedule_producers".to_string(), producers_list.join(", "))]);
+    let active_producer = block.clone().header.unwrap().producer;
+    let labels = HashMap::from([("active producer".to_string(), active_producer.clone())]);
     prom_ops.push(Counter::from("blocks_produced").with(labels.clone()).inc());
 
+    for producer in block.clone().active_schedule_v2.unwrap().producers {
+        let label = HashMap::from([("producer".to_string() , producer.account_name.clone())]);
+        prom_ops.push(Counter::from("active_schedule").with(label.clone()).inc());
+    }
+
+    prom_ops.push(Counter::from("all_blocks_produced").inc());
     Ok(prom_ops)
 }
